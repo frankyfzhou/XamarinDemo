eb86ada4c6512fdb76c7662b27d2a7af4224fdf5
AVANADE-CORP\frank.zhou <frank.zhou@avanade.com>
2018-04-11 11:32:04 -0400

    add Android Native and Xamarin.Android projects

---
+++ XamarinAndroidDemo/XamarinAndroidDemo/MainActivity.cs
@@ -0,0 +1,20 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+
+namespace XamarinAndroidDemo
+{
+    [Activity(Label = "XamarinAndroidDemo", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
